8fe4afba9e2cddcef0b60dcd2feb606a1a0f2d54
Eric Van Albert <eric@ericslaptop.(none)>
2014-02-21 09:49:23 -0500

    Working!

--- orc.py
+++ orc.py
@@ -10,9 +10,7 @@ class OrcBoard:
 		self.addr=addr
 		self.base_port=base_port
 		self.transaction_id=0
-		self.tx_socket=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-		self.rx_socket=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-		self.rx_socket.bind(('',self.base_port))
+		self.sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
 	def get_version(self):
 		resp=self.do_command(0x0002)
@@ -36,18 +34,18 @@ class OrcBoard:
 
 	def get_status(self):
 		resp=self.do_command(0x0001)
-		if len(resp) != 252:
+		if len(resp) != 205:
 			raise Exception('Improperly sized response')
 		
-		result=struct.unpack(
+		result=struct.unpack('>'+
 			'IH'+ # Status flags, debug chars waiting 
 			'HHH'*13+ # Raw Analog (value, filtered value, filter alpha)
 			'II'+ # Digital (value, direction)
-			'BhhH'*4+ # Motors (enable, actual pwm, goal pwm, slew rate)
+			'BhhH'*3+ # Motors (enable, actual pwm, goal pwm, slew rate)
 			'ii'*2+ # Encoders (position, velocity)
 
 			'BI'*8+ # Fast Digital IO (mode, config)
-			'lI'*3, # Gyro (integral, count)
+			'qI'*3, # Gyro (integral, count)
 			resp)
 		out={
 			'status_flags':result[0],
@@ -55,10 +53,10 @@ class OrcBoard:
 			'raw_analog':[{'value':result[i],'filtered_value':result[i+1],'filter_alpha':result[i+2]} for i in range(2,41,3)],
 			'digital_value':result[41],
 			'digital_direction':result[42],
-			'motors':[{'enable':result[i],'actual_pwm':result[i+1],'goal_pwm':result[i+2],'slew_rate':result[i+3]} for i in range(43,59,4)],
-			'encoders':[{'position':result[i],'velocity':result[i+1]} for i in range(59,63,2)],
-			'fast_digital_io':[{'mode':result[i],'config':result[i+1]} for i in range(63,79,2)],
-			'gyro':[{'integral':result[i],'count':[i+1]} for i in range(79,85)],
+			'motors':[{'enable':result[i],'actual_pwm':result[i+1],'goal_pwm':result[i+2],'slew_rate':result[i+3]} for i in range(43,55,4)],
+			'encoders':[{'position':result[i],'velocity':result[i+1]} for i in range(55,59,2)],
+			'fast_digital_io':[{'mode':result[i],'config':result[i+1]} for i in range(59,75,2)],
+			'gyro':[{'integral':result[i],'count':result[i+1]} for i in range(75,81,2)],
 		}
 		return out
 
@@ -73,24 +71,32 @@ class OrcBoard:
 	def do_command(self,command_id,payload='',rx_bufsize=1600):
 		tid=self.get_transaction_id()
 		t=self.get_time_ms()
-		cmd=struct.pack('IILI',self.HEADER,tid,t,command_id)
+		cmd=struct.pack('>IIQI',self.HEADER,tid,t,command_id)
 		cmd+=payload
 		port=self.base_port+((command_id>>24)&0xFF) # wtf
-  		self.tx_socket.sendto(cmd, (self.addr, port))
+  		self.sock.sendto(cmd, (self.addr, port))
 
-		data,addr=self.rx_socket.recvfrom(rx_bufsize)
+		data,addr=self.sock.recvfrom(rx_bufsize)
 		if len(data)<20:
 			raise Exception("Bad response from Orc: not enough data")
 		header=data[0:20]
 		result=data[20:]
-		signature,rx_tid,rx_t,response_id=struct.unpack('IILI',header)
+		signature,rx_tid,rx_t,response_id=struct.unpack('>IIQI',header)
 		if signature != self.SIGNATURE:
 			raise Exception("Bad signature from Orc")
-		#print rx_tid,rx_t,response_id
+		if rx_tid != tid:
+			raise Exception("Received response out of order")
 		return result
 
-o=OrcBoard('localhost')
-o.SIGNATURE=o.HEADER
-o.get_status()
-#print repr(o.get_version())
+if __name__=='__main__':
+	import time
+	o=OrcBoard()
+	print "VERSION ",o.get_version()
+	while True:
+		o.set_motor(0,100) # Left wheel forward
+		o.set_motor(1,-100) # Right wheel forward
+		status=o.get_status()
+		print status['encoders'][0]
+		print status['encoders'][1]
+		time.sleep(0.01)
 
